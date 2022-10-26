7461f5b4cdafce802dbd0b13df864338b06ed842
Jorge Arbelaez Carmona <you@example.com>
2022-10-26 01:19:42 -0500

    administrador de citas-React

---
+++ src/components/Listado-pacientes.jsx
@@ -0,0 +1,16 @@
+
+
+const ListadoPacientes = () => {
+
+
+
+  return (
+    <div>
+        <h2 className="w-1/2 lg:w-3/5">Listado-pacientes</h2>
+
+
+    </div>
+  )
+}
+
+export default ListadoPacientes
